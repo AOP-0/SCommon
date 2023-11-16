9c9c5821553c2e61ca95c81af4ace2ba2aae98c8
AOP <AOP-0@163.com>
2023-11-16 17:25:55 +0800

    no message

---
+++ Sources/SwiftCommon/AudioAnalyze.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by  on 2023/11/17.
+//
+
+import Foundation
--- Sources/SwiftCommon/SwiftRuntime.swift
+++ Sources/SwiftCommon/SwiftRuntime.swift
@@ -1,8 +1,36 @@
-//
-//  File.swift
-//  
-//
-//  Created by  on 2023/11/16.
-//
 
 import Foundation
+
+
+public struct SwiftRuntime {
+    
+    public static func swizzing(sel: Selector,
+                         rSel: Selector,
+                         class classType: AnyClass) {
+        let select1 = sel
+        let select2 = rSel
+        
+        guard let select1Method = class_getInstanceMethod(classType, select1) else {
+            return
+        }
+        
+        guard let select2Method = class_getInstanceMethod(classType, select2) else {
+            return
+        }
+        
+        let didAddMethod = class_addMethod(classType,
+                                           select1,
+                                           method_getImplementation(select2Method),
+                                           method_getTypeEncoding(select2Method))
+        if didAddMethod {
+            class_replaceMethod(classType,
+                                select2,
+                                method_getImplementation(select1Method),
+                                method_getTypeEncoding(select1Method))
+        } else {
+            method_exchangeImplementations(select1Method, select2Method)
+        }
+    }
+}
+
+
